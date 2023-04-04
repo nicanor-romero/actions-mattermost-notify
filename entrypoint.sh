3a0a58beb65568d0c531644526492a083bd09154
Nicanor Romero Venier <nicanor.romero@masmovil.com>
2023-04-04 13:05:47 +0200

    Update params

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/sh -l
 
-echo "Starting entrypoint"
-echo "1: $1"
-echo "2: $2"
-echo "3: $3"
-echo "4: $4"
-echo "5: $5"
-echo "6: $6"
-
-MATTERMOST_PERSONAL_ACCESS_TOKEN="$1" \
-COMMIT_URL="$2" \
-COMMIT_AUTHOR_USERNAME="$3" \
-COMMIT_AUTHOR_EMAIL="$4" \
-COMMIT_MESSAGE="$5" \
-TEST_JOB_OUTPUT=$6 \
+MATTERMOST_ACCESS_TOKEN='$1' \
+REPO_URL='$2' \
+COMMIT_SHA='$3' \
+CHECK_RUN_NAME='$4' \
+CHECK_RUN_CONCLUSION='$5' \
+CHECK_RUN_URL='$6' \
+CHECK_RUN_OUTPUT_TITLE='$7' \
+CHECK_RUN_OUTPUT_TEXT='$8' \
+CHECK_RUN_OUTPUT_SUMMARY='$9' \
 go run /main.go
--- main.go
+++ main.go
@@ -1,88 +1,77 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 type Commit struct {
+	sha            string
 	url            string
 	authorUsername string
 	authorEmail    string
 	commitMessage  string
 }
 
-type JobOutput struct {
-	Outputs    map[string]string `json:"outputs"`
-	Outcome    string            `json:"outcome"`
-	Conclusion string            `json:"conclusion"`
+type CheckRun struct {
+	Name          string
+	Conclusion    string
+	Url           string
+	OutputTitle   string
+	OutputText    string
+	OutputSummary string
 }
 
-func (o JobOutput) Failed() bool {
-	return o.Outcome == "failure"
+func (o CheckRun) Succeeded() bool {
+	return o.Conclusion == "success"
+}
+
+func (o CheckRun) Failed() bool {
+	return o.Conclusion == "failure"
 }
 
 func main() {
-	accessToken := os.Getenv("MATTERMOST_PERSONAL_ACCESS_TOKEN")
+	accessToken := os.Getenv("MATTERMOST_ACCESS_TOKEN")
 
 	client := model.NewAPIv4Client("https://mattermost.masstack.com")
 	client.SetToken(accessToken)
 
 	commit := Commit{
-		url:            os.Getenv("COMMIT_URL"),
-		authorUsername: os.Getenv("COMMIT_AUTHOR_USERNAME"),
-		authorEmail:    os.Getenv("COMMIT_AUTHOR_EMAIL"),
-		commitMessage:  os.Getenv("COMMIT_MESSAGE"),
+		sha: os.Getenv("COMMIT_SHA"),
+		url: os.Getenv("REPO_URL") + "/commit/" + os.Getenv("COMMIT_SHA"),
+
+		// TODO: Get the rest via Github API
+		authorUsername: "missing_username",
+		authorEmail:    "missing_email",
+		commitMessage:  "missing_message",
 	}
 
-	fmt.Println("commit.url:", commit.url)
-	fmt.Println("commit.authorUsername:", commit.authorUsername)
-	fmt.Println("commit.authorEmail:", commit.authorEmail)
-	fmt.Println("commit.commitMessage:", commit.commitMessage)
-
-	testJobOutputStr := os.Getenv("TEST_JOB_OUTPUT")
-	fmt.Println("TEST_JOB_OUTPUT:", testJobOutputStr)
-	testJobOutputStr = testJobOutputStr[1 : len(testJobOutputStr)-1]
-	fmt.Println("TEST_JOB_OUTPUT:", testJobOutputStr)
-	testJobOutputStr = strings.ReplaceAll(testJobOutputStr, "\\n", "")
-	fmt.Println("TEST_JOB_OUTPUT:", testJobOutputStr)
-	testJobOutputStr = strings.ReplaceAll(testJobOutputStr, "\\", "\"")
-	fmt.Println("TEST_JOB_OUTPUT:", testJobOutputStr)
-
-	testJobOutput := make(map[string]JobOutput, 0)
-	err := json.Unmarshal([]byte(testJobOutputStr), &testJobOutput)
-	if err != nil {
-		panic(err)
+	checkRun := CheckRun{
+		Name:          os.Getenv("CHECK_RUN_NAME"),
+		Conclusion:    os.Getenv("CHECK_RUN_CONCLUSION"),
+		Url:           os.Getenv("CHECK_RUN_URL"),
+		OutputTitle:   os.Getenv("CHECK_RUN_OUTPUT_TITLE"),
+		OutputText:    os.Getenv("CHECK_RUN_OUTPUT_TEXT"),
+		OutputSummary: os.Getenv("CHECK_RUN_OUTPUT_SUMMARY"),
 	}
 
-	// {
-	//  "lint": {
-	//    "outputs": {},
-	//    "outcome": "failure",
-	//    "conclusion": "failure"
-	//  },
-	//  "pytest": {
-	//    "outputs": {},
-	//    "outcome": "skipped",
-	//    "conclusion": "skipped"
-	//  }
-	//}
-
-	message := fmt.Sprintf(":warning: The commit [%s](%s) by @nicanor.romero (%s - %s) has failed one or more pipeline steps:",
+	message := fmt.Sprintf(":warning: The commit [%s](%s) by @nicanor.romero (%s - %s) has failed the pipeline step %s",
 		commit.commitMessage,
 		commit.url,
 		commit.authorUsername,
 		commit.authorEmail,
+		checkRun.Name,
 	)
-	for jobKey, jobOutput := range testJobOutput {
-		if jobOutput.Failed() {
-			message += fmt.Sprintf("\n  * :red_circle: %s", jobKey)
-		}
+
+	if checkRun.Failed() {
+		message += "\n  :red_circle: "
+	} else {
+		// TODO: Delete this, only debugging
+		message += "\n  :large_green_circle: "
 	}
+	message += fmt.Sprintf("%s\n    _%s_", checkRun.OutputTitle, checkRun.OutputText)
 
 	testChannelId := "audgc68w4pri7eybkt4byg9pze"
 	post := &model.Post{
